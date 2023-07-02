d0a66daa027357a4e273cc8ce612581690141179
shinyorke <spirits.is.my.rader@gmail.com>
2023-07-02 13:18:33 +0900

    update stats url 2021 -> 2022

--- teams.py
+++ teams.py
@@ -3,8 +3,8 @@ import csv
 from requests_html import HTMLSession
 
 URLS = {
-    'PACIFIC': 'https://www.baseball-reference.com/register/league.cgi?id=16632292',
-    'CENTRAL': 'https://www.baseball-reference.com/register/league.cgi?id=0549ac26',
+    'PACIFIC': 'https://www.baseball-reference.com/register/league.cgi?id=829bfc2b',
+    'CENTRAL': 'https://www.baseball-reference.com/register/league.cgi?id=6ffa4c10',
 }
 
 session = HTMLSession()
