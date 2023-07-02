1f22b37ad50230adce5d628c29e9740c699c5900
shinyorke <spirits.is.my.rader@gmail.com>
2023-07-02 12:52:39 +0900

    update

--- concat_files.py
+++ concat_files.py
@@ -18,15 +18,15 @@ dfp = concat('player_pitcher_')
 
 # 投手と打者一覧をjoinして選手一覧を作る
 pd.concat([dfp, dfb]).to_csv('dataset/players.csv', index=False)
-
-# 打者成績一覧
+#
+# # 打者成績一覧
 df_bs = concat('stats_batting_player_batter_')
 df_bs.to_csv('dataset/stats_batting.csv', index=False)
-
-# 投手成績一覧
+#
+# # 投手成績一覧
 df_ps = concat('stats_pitching_player_pitcher_')
 df_ps.to_csv('dataset/stats_pitching.csv', index=False)
-
+#
 # 守備成績
 df_fs = concat('stats_filding_player_')
 df_fs.to_csv('dataset/stats_filding.csv', index=False)
